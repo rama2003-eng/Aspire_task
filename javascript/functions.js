17a8595b72cd23f90e6e69bf12811b9e531e8abe
RAMAPRABA J <124386294+rama2003-eng@users.noreply.github.com>
2024-03-30 09:21:07 +0530

    commit

---
+++ javascript/functions.js
@@ -0,0 +1,47 @@
+console.log("-------------Callback Function-------------");
+function func(callback) {
+    console.log("Execute Function");
+      callback();
+   
+  }
+
+  function callbackFunction() {
+    console.log("Callback function executed!");
+  }
+  
+  
+  func(callbackFunction);
+
+  console.log("-------------Anonymous Function-------------");
+  var greet = function() {
+    console.log("Hello, world!");
+  };
+  
+  greet();
+  console.log("-------------Arrow Function-------------");
+  const arrow = () => {
+    console.log("Arrow function");
+  };
+  
+  arrow();
+  console.log("-------------IIFE Function-------------");
+  
+  (function() {
+    console.log("This is an IIFE");
+  })();
+  console.log("-------------Generator Function-------------");
+  
+
+  function* generateSequence() {
+    yield 1;
+    yield 2;
+    yield 3;
+  }
+  
+  const iterator = generateSequence();
+ 
+  console.log(iterator.next()); 
+  console.log(iterator.next()); 
+  console.log(iterator.next()); 
+  console.log(iterator.next()); 
+
