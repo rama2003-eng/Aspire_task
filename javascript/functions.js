214a39c04d853dc8fe3712695fdf2c87417bf6c5
RAMAPRABA J <124386294+rama2003-eng@users.noreply.github.com>
2024-04-29 10:30:20 +0530

    Update functions.js

--- javascript/functions.js
+++ javascript/functions.js
@@ -2,54 +2,7 @@
 // Title : Functions in Javascript
 //Author :Ramapraba J
 //Create date:21-03-2024
-console.log("-------------Callback Function-------------");
-function func(callback) {
-    console.log("Execute Function");
-      callback();
-   
-  }
 
-  function callbackFunction() {
-    console.log("Callback function executed!");
-  }
-  
-  
-  func(callbackFunction);
-
-  console.log("-------------Anonymous Function-------------");
-  var greet = function() {
-    console.log("Hello, world!");
-  };
-  
-  greet();
-  console.log("-------------Arrow Function-------------");
-  const arrow = () => {
-    console.log("Arrow function");
-  };
-  
-  arrow();
-  console.log("-------------IIFE Function-------------");
-  
-  (function() {
-    console.log("This is an IIFE");
-  })();
-  console.log("-------------Generator Function-------------");
-  
-
-  function* generateSequence() {
-    yield 1;
-    yield 2;
-    yield 3;
-  }
-  
-  const iterator = generateSequence();
- 
-  console.log(iterator.next()); 
-  console.log(iterator.next()); 
-  console.log(iterator.next()); 
-  console.log(iterator.next()); 
-
-=======
 console.log("-------------Callback Function-------------");
 function func(callback) {
     console.log("Execute Function");
